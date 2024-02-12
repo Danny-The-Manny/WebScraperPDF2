95744d96dc7e1ba99772988e93d234ea4e324467
100033295 <jc100033295@gmail.com>
2024-02-12 10:36:20 -0600

    Reorg

--- main.py
+++ main.py
@@ -8,56 +8,54 @@ from bs4 import BeautifulSoup
 
 import io
 
-#from PyPDF2 import PdfFileReader
-
-# website to scrap
-url = "https://www.geeksforgeeks.org/how-to-extract-pdf-tables-in-python/"
-
-# get the url from requests get method
-read = requests.get(url)
-
-# full html content
-html_content = read.content
-
-# Parse the html content
-soup = BeautifulSoup(html_content, "html.parser")
-
-#This is the getting pdf part
-
-# created an empty list for putting the pdfs
-list_of_pdf = set()
-
-# accessed the first p tag in the html
-l = soup.find('p')
-
-# accessed all the anchors tag from given p tag
-p = l.find_all('a')
-
-# iterate through p for getting all the href links
-for link in p:
-    # original html links
-    print("links: ", link.get('href'))
-    print("\n")
-
-    # converting the extension from .html to .pdf
-    pdf_link = (link.get('href')[:-5]) + ".pdf"
-
-    # converted to .pdf
-    print("converted pdf links: ", pdf_link)
-    print("\n")
-
-    # added all the pdf links to set
-    list_of_pdf.add(pdf_link)
+# from PyPDF2 import PdfFileReader
 
 
 def print_hi(name):
-     #Use a breakpoint in the code line below to debug your script.
+    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # website to scrap
+    url = "https://www.geeksforgeeks.org/how-to-extract-pdf-tables-in-python/"
+
+    # get the url from requests get method
+    read = requests.get(url)
+
+    # full html content
+    html_content = read.content
+
+    # Parse the html content
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # This is the getting pdf part
+
+    # created an empty list for putting the pdfs
+    list_of_pdf = set()
+
+    # accessed the first p tag in the html
+    l = soup.find('p')
+
+    # accessed all the anchors tag from given p tag
+    p = l.find_all('a')
+
+    # iterate through p for getting all the href links
+    for link in p:
+        # original html links
+        print("links: ", link.get('href'))
+        print("\n")
+
+        # converting the extension from .html to .pdf
+        pdf_link = (link.get('href')[:-5]) + ".pdf"
+
+        # converted to .pdf
+        print("converted pdf links: ", pdf_link)
+        print("\n")
+
+        # added all the pdf links to set
+        list_of_pdf.add(pdf_link)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
