b86064affa50fa29c138eeb08e649a5beb9bd1c8
100033295 <jc100033295@gmail.com>
2024-02-27 10:40:34 -0600

    Removed random error.

--- main.py
+++ main.py
@@ -55,13 +55,14 @@ while True:
                     # repeat previous steps on the link variable
                     read = requests.get(base_link)
                     html_content = read.content
-                    new_soup = BeautifulSoup(html_content, "html.parser")
-                    g = soup.find_all('a')
-                    # Tests to see if the link is a PDF
+                    # Break from repeating previous steps to test to see if the link is a PDF
                     if ".pdf" in base_link:
                         list_of_pdf.add(base_link)
                         print(base_link)
                     else:
+                        # Resume repeating previous steps
+                        new_soup = BeautifulSoup(html_content, "html.parser")
+                        g = soup.find_all('a')
                         for new_link in g:
                             try:
                                 new_href_link = link.get('href')
